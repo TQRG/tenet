930682c80870bfa0334eacfa86670629f449d393
epicosy <xtremedy@gmail.com>
2022-01-17 15:32:29 +0000

    filter plugin: copy filtered files to new directory

--- securityaware/plugins/filter.py
+++ securityaware/plugins/filter.py
@@ -11,6 +11,7 @@ class FilterHandler(PluginHandler):
     """
         Separate plugin
     """
+
     class Meta:
         label = "filter"
 
@@ -27,23 +28,52 @@ class FilterHandler(PluginHandler):
         self.app.log.warning(f"File {file} not found.")
         return False
 
+    def copy_files(self, a_proj: str, a_path: str, a_file: Path, b_proj: Path, b_path: Path, b_file: Path,
+                   dest_path: Path):
+        a_new_file = dest_path / a_proj / a_path
+        a_new_file.parent.mkdir(parents=True, exist_ok=True)
+        a_path = Path(self.get('files_path'), a_proj, a_path)
+
+        with a_path.open(mode="r") as rf, a_new_file.open(mode="w") as wf:
+            self.app.log.info(f"Writing {a_file} to {a_new_file}.")
+            wf.write(rf.read())
+
+        b_new_file = dest_path / b_proj / b_path
+        b_new_file.parent.mkdir(parents=True, exist_ok=True)
+        b_path = Path(self.get('files_path'), b_proj, b_path)
+
+        with b_path.open(mode="r") as rf, b_new_file.open(mode="w") as wf:
+            self.app.log.info(f"Writing {b_file} to {b_new_file}.")
+            wf.write(rf.read())
+
     def run(self, dataset: pd.DataFrame, file_size: int = None, **kwargs) -> Union[pd.DataFrame, None]:
         """
             runs the plugin
         """
         self.set('dataset', self.output)
+        out_files_path = Path(self.path, 'files')
+        self.set('files_path', out_files_path)
 
         if file_size:
             filtered_dataset = []
 
             for i, row in dataset.iterrows():
-                if self.is_file_within(Path(row.a_file), file_size) and self.is_file_within(Path(row.b_file), file_size):
+                if self.is_file_within(Path(row.a_file), file_size) and self.is_file_within(Path(row.b_file),
+                                                                                            file_size):
                     filtered_dataset.append(row)
+                    self.copy_files(a_proj=row.a_proj, a_path=row.a_path, a_file=row.a_file,
+                                    b_proj=row.b_proj, b_path=row.b_path, b_file=row.b_file,
+                                    dest_path=out_files_path)
 
             if filtered_dataset:
                 return pd.DataFrame(filtered_dataset, columns=list(dataset.columns.values))
 
             return None
+
+        for i, row in dataset.iterrows():
+            self.copy_files(a_proj=row.a_proj, a_path=row.a_path, a_file=row.a_file,
+                            b_proj=row.b_proj, b_path=row.b_path, b_file=row.b_file,
+                            dest_path=out_files_path)
 
         return dataset
 
