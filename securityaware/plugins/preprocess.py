ed42dd6313bed814a782d0146a77db6a29a3bc13
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2022-08-24 11:49:52 +0100

    plugin for preprocessing dataset for code2vec

--- securityaware/plugins/preprocess.py
+++ securityaware/plugins/preprocess.py
@@ -3,66 +3,119 @@ import pandas as pd
 from typing import Union
 from pathlib import Path
 
-from securityaware.data.output import CommandData
+from securityaware.core.astminer.common import common
+from securityaware.core.astminer.preprocess import process_file, save_dictionaries
 from securityaware.handlers.plugin import PluginHandler
 
 
-class HistogramHandler(PluginHandler):
+class PreprocessHandler(PluginHandler):
     """
-        Histogram plugin
+        Preprocess plugin
     """
 
     class Meta:
-        label = "histogram"
+        label = "preprocess"
 
     def __init__(self, **kw):
         super().__init__(**kw)
 
-    (["-tdf", "--train_data_file"], {'help': 'The train dataset file.', 'required': True}),
-    (["-thf", "--target_hist_file"], {'help': 'Target histogram output file.', 'required': True}),
-    (["-ohf", "--origin_hist_file"], {'help': 'Origin histogram output file.', 'required': True}),
-    (["-phf", "--path_hist_file"], {'help': 'Path histogram output file.', 'required': True}),
-
-    def run(self, dataset: pd.DataFrame, **kwargs) -> Union[pd.DataFrame, None]:
+    def run(self, dataset: pd.DataFrame, max_contexts: int = 200, word_vocab_size: int = 1301136,
+            path_vocab_size: int = 911417, target_vocab_size: int = 261245, **kwargs) -> Union[pd.DataFrame, None]:
         """
-            runs the plugin
+            Parameters:
+                max_contexts (int): number of max contexts to keep
+                word_vocab_size (int): Max number of origin word in to keep in the vocabulary
+                path_vocab_size (int): Max number of paths to keep in the vocabulary
+                target_vocab_size (int): Max number of target words to keep in the vocabulary
         """
 
-        if not self.get('train_data_file'):
+        dataset_name = self.output.stem
+        train_output_path = f'{self.path}/{dataset_name}.train.c2v'
+        self.set('train_output_path', train_output_path)
+        val_output_path = f'{self.path}/{dataset_name}.val.c2v'
+        self.set('val_output_path', val_output_path)
+        test_output_path = f'{self.path}/{dataset_name}.test.c2v'
+        self.set('test_output_path', test_output_path)
+        dict_file_path = Path(self.path, f'{dataset_name}.dict.c2v')
+        self.set('dict_file_path', dict_file_path)
+
+        train_data_path = self.get('train_data_path')
+        val_data_path = self.get('val_data_path')
+        test_data_path = self.get('test_data_path')
+        word_histogram_path = self.get('word_histogram_path')
+        path_histogram_file = self.get('path_histogram_file')
+        target_histogram_path = self.get('target_histogram_path')
+
+        if not train_data_path:
             self.app.log.warning(f"Train data file not instantiated.")
             return None
 
-        if not Path(self.get('train_data_file')).exists():
+        if not Path(train_data_path).exists():
             self.app.log.warning(f"Train data file not found.")
             return None
 
-        target_hist_file = Path(self.path, 'target.hist.txt')
-        origin_hist_file = Path(self.path, 'origin.hist.txt')
-        path_hist_file = Path(self.path, 'path.hist.txt')
+        if not val_data_path:
+            self.app.log.warning(f"Validation data file not instantiated.")
+            return None
 
-        self.set('target_hist_file', target_hist_file)
-        self.set('origin_hist_file', origin_hist_file)
-        self.set('path_hist_file', path_hist_file)
+        if not Path(val_data_path).exists():
+            self.app.log.warning(f"Validation data file not found.")
+            return None
 
-        command_handler = self.app.handler.get('handlers', 'command', setup=True)
+        if not test_data_path:
+            self.app.log.warning(f"Test data file not instantiated.")
+            return None
 
-        # histogram of the labels
-        cmd_args = f"cat {self.get('train_data_file')} | cut -d' ' -f1"
-        cmd_args += " | awk '{n[$0]++} END {for (i in n) print i,n[i]}' > " + str(target_hist_file)
-        command_handler(cmd_data=CommandData(args=cmd_args))
+        if not Path(test_data_path).exists():
+            self.app.log.warning(f"Test data file not found.")
+            return None
 
-        # histogram of all source/target words
-        cmd_args = f"cat {self.get('train_data_file')} | cut -d' ' -f2 - | tr ' ' '\\n' | cut -d ',' -f1-3 | tr ',' '\\n'"
-        cmd_args += " | awk '{n[$0]++} END {for (i in n) print i,n[i]}' > " + str(origin_hist_file)
-        command_handler(cmd_data=CommandData(args=cmd_args))
+        if not word_histogram_path:
+            self.app.log.warning(f"Word histogram data file not instantiated.")
+            return None
 
-        # histogram of all the path hashes
-        cmd_args = f"cat {self.get('train_data_file')} | cut -d ' ' -f2 - | tr ' ' '\\n' | cut -d ',' -f2"
-        cmd_args += " | awk '{n[$0]++} END {for (i in n) print i,n[i]}' > " + str(path_hist_file)
-        command_handler(cmd_data=CommandData(args=cmd_args))
+        if not Path(word_histogram_path).exists():
+            self.app.log.warning(f"Word histogram data file not found.")
+            return None
+
+        if not path_histogram_file:
+            self.app.log.warning(f"Path histogram data file not instantiated.")
+            return None
+
+        if not Path(path_histogram_file).exists():
+            self.app.log.warning(f"Path histogram data file not found.")
+            return None
+
+        if not target_histogram_path:
+            self.app.log.warning(f"Target histogram data file not instantiated.")
+            return None
+
+        if not Path(target_histogram_path).exists():
+            self.app.log.warning(f"Target histogram data file not found.")
+            return None
+
+        word_to_count, path_to_count, target_to_count = common.get_counts(word_histogram=word_histogram_path,
+                                                                          word_vocab_size=word_vocab_size,
+                                                                          path_histogram=path_histogram_file,
+                                                                          path_vocab_size=path_vocab_size,
+                                                                          target_histogram=target_histogram_path,
+                                                                          target_vocab_size=target_vocab_size)
+
+        num_training_examples = process_file(file_path=train_data_path, output_path=train_output_path,
+                                             word_to_count=word_to_count, path_to_count=path_to_count,
+                                             max_contexts=max_contexts)
+
+        process_file(file_path=test_data_path, output_path=test_output_path, word_to_count=word_to_count,
+                     path_to_count=path_to_count, max_contexts=max_contexts)
+
+        process_file(file_path=val_data_path, output_path=val_output_path, word_to_count=word_to_count,
+                     path_to_count=path_to_count, max_contexts=max_contexts)
+
+        save_dictionaries(dict_file_path=dict_file_path, word_to_count=word_to_count, path_to_count=path_to_count,
+                          target_to_count=target_to_count, num_training_examples=num_training_examples)
 
         return dataset
 
 
 def load(app):
-    app.handler.register(HistogramHandler)
+    app.handler.register(PreprocessHandler)
