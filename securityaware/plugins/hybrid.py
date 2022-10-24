ffa03ddfc8f279c1809146f183bc34acbd33cf73
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2022-10-24 13:04:05 +0100

    inner join for unsafe label

--- securityaware/plugins/hybrid.py
+++ securityaware/plugins/hybrid.py
@@ -15,9 +15,11 @@ class HybridHandler(PluginHandler):
     def __init__(self, **kw):
         super().__init__(**kw)
 
-    def run(self, dataset: pd.DataFrame, **kwargs) -> Union[pd.DataFrame, None]:
+    def run(self, dataset: pd.DataFrame, inner_join: bool = False, **kwargs) -> Union[pd.DataFrame, None]:
         """
             runs the plugin
+            :param inner_join: flag to perform inner join between diff labelled and static labelled datasets, if false,
+            full outer join if performed
         """
 
         diff_labelled_data_path = self.get('diff_labelled_data_path')
@@ -37,11 +39,18 @@ class HybridHandler(PluginHandler):
         # label the safe labels in the diff dataset as unsafe given
         unsafe_static_labelled_fns = static_labelled_data[static_labelled_data.label == 'unsafe'].hash.to_list()
 
-        for i, row in diff_labelled_data[diff_labelled_data.hash.isin(unsafe_static_labelled_fns)].iterrows():
-            if row.label != 'unsafe':
-                self.app.log.info(f"Updating label for {row.fpath}")
-                diff_labelled_data.at[i, 'label'] = 'unsafe'
-        self.app.log.info(f"Unsafe fns: {len(diff_labelled_data[diff_labelled_data.label == 'unsafe'])}")
+        if inner_join:
+            for i, row in diff_labelled_data[diff_labelled_data.label == 'unsafe'].iterrows():
+                if row.label == 'unsafe' and row.hash not in unsafe_static_labelled_fns:
+                    self.app.log.info(f"Updating label for {row.fpath}")
+                    diff_labelled_data.at[i, 'label'] = 'safe'
+        else:
+            for i, row in diff_labelled_data[diff_labelled_data.hash.isin(unsafe_static_labelled_fns)].iterrows():
+                if row.label != 'unsafe':
+                    self.app.log.info(f"Updating label for {row.fpath}")
+                    diff_labelled_data.at[i, 'label'] = 'unsafe'
+            self.app.log.info(f"Unsafe fns: {len(diff_labelled_data[diff_labelled_data.label == 'unsafe'])}")
+
         return diff_labelled_data
 
 
