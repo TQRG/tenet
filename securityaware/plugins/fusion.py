45fbea706cf90d4d70dbe598718a086c28cdf7ac
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2022-10-25 19:05:27 +0100

    fixed label leakage, labels are different in common_fns; added fusion to config

--- securityaware/plugins/fusion.py
+++ securityaware/plugins/fusion.py
@@ -36,43 +36,44 @@ class FusionHandler(PluginHandler):
 
         diff_labelled_data = pd.read_csv(str(diff_labelled_data_path))
         static_labelled_data = pd.read_csv(str(static_labelled_data_path))
-        diff_labelled_data.reset_index(inplace=True)
-        static_labelled_data.reset_index(inplace=True)
+        diff_labelled_data.reset_index(inplace=True, drop=True)
+        static_labelled_data.rename(columns={'label': 'da_label'}, inplace=True)
+        static_labelled_data.reset_index(inplace=True, drop=True)
+        static_labelled_data.rename(columns={'label': 'sa_label'}, inplace=True)
 
         diff_data_hashes = diff_labelled_data.hash.to_list()
         static_data_hashes = static_labelled_data.hash.to_list()
 
         # fusing diff and static datasets into a single dataset
-        common_fns = diff_labelled_data[diff_labelled_data.hash.isin(static_data_hashes)]
+        common_fns = pd.merge(diff_data_hashes, static_labelled_data[['hash', 'sa_label']], on='hash', how='left')
+
         self.app.log.info(f"Common functions {len(common_fns)}")
+        self.app.log.info(f"\tDiff safe functions: {len(common_fns[common_fns['da_label'] == 'safe'])}, "
+                          f"Diff unsafe functions: {len(common_fns[common_fns['da_label'] == 'unsafe'])}")
+        self.app.log.info(f"\tStatic safe functions: {len(common_fns[common_fns['sa_label'] == 'safe'])}, "
+                          f"Static unsafe functions: {len(common_fns[common_fns['sa_label'] == 'unsafe'])}")
 
         diff_uncommon_fns = diff_labelled_data[~diff_labelled_data.hash.isin(static_data_hashes)]
-        diff_uncommon_fns.rename(columns={'label': 'da_label'}, inplace=True)
         diff_safe_uncommon_fns = diff_uncommon_fns[diff_uncommon_fns['da_label'] == 'safe']
         diff_unsafe_uncommon_fns = diff_uncommon_fns[diff_uncommon_fns['da_label'] == 'unsafe']
         self.app.log.info(f"Diff analysis uncommon functions {len(diff_uncommon_fns)} "
                           f"({len(diff_safe_uncommon_fns)} safe, {len(diff_unsafe_uncommon_fns)} unsafe)")
 
-        diff_safe_uncommon_fns['sa_label'] = diff_safe_uncommon_fns['da_label']
-        diff_unsafe_uncommon_fns['sa_label'] = diff_unsafe_uncommon_fns['da_label'].apply(lambda x: 'safe')
+        diff_safe_uncommon_fns['sa_label'] = diff_safe_uncommon_fns['da_label'].copy()
+        diff_unsafe_uncommon_fns['sa_label'] = diff_unsafe_uncommon_fns['da_label'].copy().apply(lambda x: 'safe')
 
         static_uncommon_fns = static_labelled_data[~static_labelled_data.hash.isin(diff_data_hashes)]
-        static_uncommon_fns.rename(columns={'label': 'sa_label'}, inplace=True)
         static_safe_uncommon_fns = static_uncommon_fns[static_uncommon_fns['sa_label'] == 'safe']
         static_unsafe_uncommon_fns = static_uncommon_fns[static_uncommon_fns['sa_label'] == 'unsafe']
         self.app.log.info(f"Static analysis uncommon functions {len(static_uncommon_fns)} "
                           f"({len(static_safe_uncommon_fns)} safe, {len(static_unsafe_uncommon_fns)} unsafe)")
 
-        static_safe_uncommon_fns['da_label'] = static_safe_uncommon_fns['sa_label']
-        static_unsafe_uncommon_fns['da_label'] = static_unsafe_uncommon_fns['sa_label'].apply(lambda x: 'safe')
-
-        common_fns.rename(columns={'label': 'da_label'}, inplace=True)
-        common_fns['sa_label'] = common_fns['da_label']
+        static_safe_uncommon_fns['da_label'] = static_safe_uncommon_fns['sa_label'].copy()
+        static_unsafe_uncommon_fns['da_label'] = static_unsafe_uncommon_fns['sa_label'].copy().apply(lambda x: 'safe')
 
         dataset = pd.concat([common_fns, diff_safe_uncommon_fns, diff_unsafe_uncommon_fns, static_safe_uncommon_fns,
                              static_unsafe_uncommon_fns])
 
-        dataset.sort_index(inplace=True)
         dataset.reset_index(inplace=True)
         self.app.log.info(f"Total functions after fusion: {len(dataset)}")
 
