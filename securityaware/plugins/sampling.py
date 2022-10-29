86f7b6a15f467a506313115c3e1b7f7aa8fcf8ac
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2022-10-29 15:42:06 +0100

    single/multiple unsafe functions per commit instead of file

--- securityaware/plugins/sampling.py
+++ securityaware/plugins/sampling.py
@@ -78,13 +78,13 @@ class SamplingHandler(PluginHandler):
         self.app.log.info(f"Dataset has {len(dataset)} samples.")
 
         if only_single:
-            for g, rows in dataset[dataset.label == 'unsafe'].groupby(['owner', 'project', 'version', 'fpath']):
+            for g, rows in dataset[dataset.label == 'unsafe'].groupby(['owner', 'project', 'version']):
                 if len(rows) > 1:
                     for i, row in rows.iterrows():
                         dataset.loc[i, 'label'] = 'safe'
 
         elif only_multiple:
-            for g, rows in dataset[dataset.label == 'unsafe'].groupby(['owner', 'project', 'version', 'fpath']):
+            for g, rows in dataset[dataset.label == 'unsafe'].groupby(['owner', 'project', 'version']):
                 if len(rows) == 1:
                     for i, row in rows.iterrows():
                         dataset.loc[i, 'label'] = 'safe'
