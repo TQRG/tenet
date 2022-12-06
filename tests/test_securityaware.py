6c6f3df7dda78db9cd64307f2313e44df24e04f0
sofiaoreis <sofiareis1994@gmail.com>
2022-12-06 21:00:35 +0000

    remove dummy test

--- tests/test_securityaware.py
+++ tests/test_securityaware.py
@@ -16,21 +16,3 @@ def test_securityaware_debug():
         app.run()
         assert app.debug is True
 
-# TODO: Fix test
-# def test_command1():
-#     # test command1 without arguments
-#     argv = ['command1']
-    # with SecurityAwareTest(argv=argv) as app:
-    #     app.run()
-    #     data,output = app.last_rendered
-    #     assert data['foo'] == 'bar'
-    #     assert output.find('Foo => bar')
-
-
-    # # test command1 with arguments
-    # argv = ['command1', '--foo', 'not-bar']
-    # with SecurityAwareTest(argv=argv) as app:
-    #     app.run()
-    #     data,output = app.last_rendered
-    #     assert data['foo'] == 'not-bar'
-    #     assert output.find('Foo => not-bar')
