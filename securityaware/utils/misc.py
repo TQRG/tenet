2f65d67d01643958318db1fb2bd0713c675df5af
Sofia Reis <6732277+sofiaoreis@users.noreply.github.com>
2023-01-05 15:24:56 +0000

    working osv_source

--- securityaware/utils/misc.py
+++ securityaware/utils/misc.py
@@ -22,21 +22,6 @@ def load_json_file(file):
     """
     with open(file) as jfile:
         return json.load(jfile)
-    
-def load_dataset(path):
-    """Loads dataset if exists into a dataframe.
-    If dataset does not exist, it returns None.
-    Args:
-        path (string): path to the osv dataset
-    Returns:
-        dataframe: returns dataframe if dataframe does not exist, else None
-        bool: return True if processing the first bulk of data, else False
-    """
-    print(path)
-    if os.path.exists(path):
-        return pd.read_csv(path), False
-    else:
-        return None, True
 
 def create_df(data):
     """Create dataframe from vuln data.
