ab2a1eea0e1c4f6a8684f1c49b289b77fee2575c
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2023-01-06 16:32:15 +0000

    skip search negative samples, fixed infinit loop in the pagination class; fixed controlled and random scenarios to include missing fields from fix scenario; added tag to localgitfiles; removed redundant sample and triplet classes; runner results method also filters null fields for nested lists; sample class methods passed to code_parser handler; filter_comments method in the code_parser handler considers code language; try catch for edge case in tokenize method from code_parser handler; remove comments optional in the code_mining plugin;

--- securityaware/plugins/download.py
+++ securityaware/plugins/download.py
@@ -32,6 +32,7 @@ class Download(PluginHandler):
         self.set('dataset', self.output)
 
         self.app.log.info(f"Creating {len(dataset)} tasks.")
+
         for row in tqdm(dataset.to_dict(orient='records')):
             self.multi_task_handler.add(row=row)
 
@@ -46,7 +47,7 @@ class Download(PluginHandler):
     def download(self, row: dict):
         project_path = self.files_dir / row['project_name']
         if 'raw_url_vuln' in row and not pd.isnull(row['raw_url_vuln']):
-            vuln_file = LocalGitFile(url=row['raw_url_vuln'], short=Path(row['file_path']),
+            vuln_file = LocalGitFile(url=row['raw_url_vuln'], short=Path(row['file_path']), tag='vuln',
                                      path=project_path / row['vuln_commit_hash'] / row['file_path'])
 
             if not vuln_file.path.exists():
@@ -56,7 +57,7 @@ class Download(PluginHandler):
                     return None
 
         if 'raw_url_fix' in row and not pd.isnull(row['raw_url_fix']):
-            fix_file = LocalGitFile(url=row['raw_url_fix'], short=Path(row['file_path']),
+            fix_file = LocalGitFile(url=row['raw_url_fix'], short=Path(row['file_path']), tag='fix',
                                     path=project_path / row['fixed_commit_hash'] / row['file_path'])
 
             if not fix_file.path.exists():
@@ -66,7 +67,7 @@ class Download(PluginHandler):
                     return None
 
         if 'raw_url_non_vuln' in row:
-            non_vuln_file = LocalGitFile(url=row["raw_url_non_vuln"], short=Path(row['non_vuln_file_path']),
+            non_vuln_file = LocalGitFile(url=row["raw_url_non_vuln"], short=Path(row['non_vuln_file_path']), tag='non-vuln',
                                          path=project_path / row['non_vuln_commit_hash'] / row['non_vuln_file_path'])
             if not non_vuln_file.path.exists():
                 non_vuln_file.download()
