bc35f9e297786d3b083a092fdc4c7d336b5d1beb
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2023-01-11 15:41:42 +0000

    Simplify run command to have less required arguments closes #51

--- tenet/controllers/base.py
+++ tenet/controllers/base.py
@@ -1,4 +1,6 @@
 import os.path
+
+import inquirer
 import schema
 import yaml
 
@@ -6,6 +8,7 @@ from pathlib import Path
 
 from cement import Controller, ex
 from tenet import __version__
+from ..core.exc import TenetError
 from ..data.schema import parse_pipeline
 
 VERSION_BANNER = """
@@ -38,45 +41,43 @@ class Base(Controller):
     @ex(
         help='Runs a workflow',
         arguments=[
-            (['-f', '--file'], {'help': 'Path to the pipeline config file', 'type': str, 'required': True}),
-            (['-d', '--dataset'], {'help': 'Path to the input csv dataset file', 'type': str, 'required': True}),
+            (['-f', '--file'], {'help': 'Path to the pipeline config file', 'type': str, 'required': False}),
+            (['-d', '--dataset'], {'help': 'Path to the input csv dataset file', 'type': str, 'required': False}),
             (['-sp', '--suppress-plot'], {'help': 'Suppresses plotting', 'action': 'store_true', 'required': False,
                                           'dest': 'suppress_plot'}),
             (['-t', '--threads'], {'help': 'Number of threads (overwrites the threads in config)', 'type': int,
                                    'required': False}),
             (['-wd', '--workdir'], {'help': 'Path to the workdir.', 'type': str, 'required': True}),
             (['-b', '--bind'], {'help': 'Docker directory path to bind (to workdir as a volume).',
-                                'type': str, 'required': True})
+                                'type': str, 'required': False})
         ]
     )
     def run(self):
-        file = Path(self.app.pargs.file)
-        dataset = Path(self.app.pargs.dataset)
-        self.app.threads = self.app.pargs.threads if self.app.pargs.threads else self.app.get_config('local_threads')
-
-        if not file.exists():
-            self.app.log.error(f"File {file} not found.")
-            exit(1)
-
-        if not dataset.exists():
-            self.app.log.error(f"Dataset {dataset} not found.")
-            exit(1)
-
         self.app.extend('workdir', Path(self.app.pargs.workdir))
 
         if not self.app.workdir.exists():
             self.app.workdir.mkdir(parents=True)
             self.app.log.info(f"Created workdir {self.app.workdir}")
 
-        self.app.extend('bind', self.app.pargs.bind)
+        dataset = self._parse_dataset()
+        pipeline = self._parse_pipeline()
+
+        self.app.threads = self.app.pargs.threads if self.app.pargs.threads else self.app.get_config('local_threads')
+
+        if self.app.pargs.bind:
+            bind = self.app.pargs.bind
+        else:
+            bind = str(self.app.workdir.absolute()).replace(str(Path.home()), '')
+
+        self.app.extend('bind', bind)
 
         #if not os.path.exists(self.app.bind):
         #    self.app.log.error(f"Bind path {self.app.bind} is not a valid directory path.")
         #    exit(1)
 
-        with file.open(mode="r") as stream:
+        with pipeline.open(mode="r") as stream:
             try:
-                self.app.log.info(f"Parsing pipeline file: {file}")
+                self.app.log.info(f"Parsing pipeline file: {pipeline}")
                 pipeline = parse_pipeline(yaml.safe_load(stream))
             except schema.SchemaError as se:
                 self.app.log.error(str(se))
@@ -86,3 +87,48 @@ class Base(Controller):
             workflow_handler = self.app.handler.get('handlers', 'workflow', setup=True)
             workflow_handler.load(dataset)
             workflow_handler(dataset)
+
+    def _parse_dataset(self) -> Path:
+        if self.app.pargs.dataset:
+            dataset = Path(self.app.pargs.dataset)
+        else:
+            dataset_files = [f for f in Path(self.app.pargs.workdir).iterdir() if f.suffix in ['.csv', '.tsv']]
+
+            if len(dataset_files) == 0:
+                raise TenetError('No dataset file found in the specified working directory')
+            elif len(dataset_files) == 1:
+                self.app.log.info(f"Using {dataset_files[0]} as dataset...")
+                dataset = Path(dataset_files[0])
+            else:
+                option = [
+                    inquirer.List('dataset', message="Select the dataset you want to use:", choices=dataset_files,
+                                  ),
+                ]
+                answer = inquirer.prompt(option)
+                dataset = Path(answer["dataset"])
+
+        if not dataset.exists():
+            raise TenetError(f"Dataset {dataset} not found.")
+
+        return dataset
+
+    def _parse_pipeline(self) -> Path:
+        if self.app.pargs.file:
+            pipeline = Path(self.app.pargs.file)
+        else:
+            pipeline_files = [f for f in Path(self.app.pargs.workdir).iterdir() if f.suffix in ['.yml', '.yaml']]
+            if len(pipeline_files) == 0:
+                raise TenetError('No pipeline file found in the specified working directory')
+            elif len(pipeline_files) == 1:
+                self.app.log.info(f"Using {pipeline_files[0]} as pipeline...")
+                pipeline = Path(pipeline_files[0])
+            else:
+                option = [inquirer.List('pipeline_files', message="Select the pipeline you want to run:",
+                                        choices=pipeline_files)]
+                answer = inquirer.prompt(option)
+                pipeline = Path(answer["pipeline_files"])
+
+        if not pipeline.exists():
+            raise TenetError(f"File {pipeline} not found.")
+
+        return pipeline
