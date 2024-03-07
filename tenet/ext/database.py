073f95274fdb685b72a8f245470f899fc074108c
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-03-06 17:56:37 -0800

    integrate arepo as the vulnerability database;

--- tenet/ext/database.py
+++ tenet/ext/database.py
@@ -1,18 +1,15 @@
 from sqlalchemy.exc import OperationalError
 
-from tenet.data.database import Database
+from arepo.db import DatabaseConnection as Database
 from tenet.core.exc import TenetError
 
 
 def init_database(app):
     try:
-        database = Database(dialect=app.get_config('dialect'), username=app.get_config('username'),
-                            password=app.get_config('password'), host=app.get_config('host'),
-                            port=app.get_config('port'), database=app.get_config('database'),
-                            debug=app.config.get('log.colorlog', 'database'))
-
+        app.log.info(f"Connecting to database")
+        uri = app.config.get('database', 'uri')
+        database = Database(uri)
         app.extend('db', database)
-        app.log.info(f"Connected to {app.get_config('dialect')}://{app.get_config('username')}@{app.get_config('host')}:{app.get_config('port')}/{app.get_config('database')}")
     except OperationalError as oe:
         raise TenetError(oe)
 
