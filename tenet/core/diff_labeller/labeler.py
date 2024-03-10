985d0b8b11d5b3205cbdc0359fa218ee4bf4f971
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-03-09 19:21:37 -0800

    excluded sim_ratio, line_nums, inline_diffs, and other related methods/objects from Labeler and refacotred for optional formatting; get_row_diff_range returns a list of Change objects for easier interaction;

--- tenet/core/diff_labeller/labeler.py
+++ tenet/core/diff_labeller/labeler.py
@@ -1,186 +1,126 @@
 import difflib
-import jsbeautifier
-import hashlib
 
-from pathlib import Path
-from typing import List, Tuple
+from typing import List
 
-from tenet.core.diff_labeller.misc import check_or_create_dir, get_range_offset, get_row_diff_range
-from tenet.data.diff import InlineDiff, Entry
+from tenet.core.diff_labeller.misc import get_range_offset, get_row_diff_range
+from tenet.data.diff import Change
 
 
 class Labeler:
-    def __init__(self, entry: Entry, a_str: str, b_str: str, inline_proj_dir: Path):
+    def __init__(self, a_str: str, b_str: str, prettify: bool = False, extension: str = None):
         """
-            :param entry: Diff block entry.
             :param a_str: The contents of file A.
             :param b_str: The contents of file B.
         """
-        self.entry = entry
-        self.a_del_line_cnt = 0
-        self.b_add_line_cnt = 0
-        self.inline_diffs: List[InlineDiff] = []
-        self.sim_ratio = None
-        self.inline_proj_dir = inline_proj_dir
+        self.a_str = a_str
+        self.b_str = b_str
+        self.prettify = prettify
+        self.extension = extension  # TODO: extract the extension from the file name
 
-        a_path_mod = self.entry.diff_block.a_path.replace('/', '_')
-        self.a_path_file = inline_proj_dir / f"a_{a_path_mod}.txt"
-        b_path_mod = self.entry.diff_block.b_path.replace('/', '_')
-        self.b_path_file = inline_proj_dir / f"b_{b_path_mod}.txt"
-        self.inline_file = inline_proj_dir / f"{a_path_mod}_inline.txt"
+        self._a_formatted = None
+        self._b_formatted = None
+        self._b_formatted_range = None
+        self._a_formatted_range = None
 
-        self.a_formatted = None
-        self.b_formatted = None
-        self.b_formatted_range = None
-        self.a_formatted_range = None
-        self.inline_diff_text = None
-        self.a_formatted_lines = None
-        self.b_formatted_lines = None
-        self.size_a_lines = None
-        self.size_b_lines = None
+        self._diff_lines = None
+        self._diff_bound = None
 
-        self.pretty_printing(a_str, b_str)
-        self.get_inline_diffs(a_str, b_str)
+    @property
+    def diff_lines(self) -> List[str]:
+        if not self._diff_lines:
+            if self.prettify:
+                self._diff_lines = list(difflib.unified_diff(self.a_formatted.splitlines(keepends=True),
+                                                             self.b_formatted.splitlines(keepends=True)))
+            else:
+                self._diff_lines = list(difflib.unified_diff(self.a_str.splitlines(keepends=True),
+                                                             self.b_str.splitlines(keepends=True)))
 
-    def diff_bound(self) -> Tuple[List[int], List[str], int]:
-        inline_lines = self.inline_diff_text.splitlines()
-        # Look for diffs
-        diff_bound = [inline_id for inline_id, inline_line in enumerate(inline_lines) if inline_line.startswith("@@ ")]
-        num_diffs = len(diff_bound)
-        diff_bound.append(len(inline_lines))
+        return self._diff_lines
 
-        return diff_bound, inline_lines, num_diffs
+    @property
+    def diff_bound(self) -> List[int]:
+        if not self._diff_bound:
+            self._diff_bound = [idx for idx, line in enumerate(self.diff_lines) if line.startswith("@@ ")]
+            self._diff_bound.append(len(self.diff_lines))
 
-    def get_inline_diffs(self, a_str: str, b_str: str):
-        self.a_formatted_range = get_range_offset(a_str, self.a_formatted)
-        self.b_formatted_range = get_range_offset(b_str, self.b_formatted)
-        self.a_formatted_lines = self.a_formatted.splitlines(keepends=True)
-        self.b_formatted_lines = self.b_formatted.splitlines(keepends=True)
-        self.size_a_lines = len(self.a_formatted_lines)
-        self.size_b_lines = len(self.b_formatted_lines)
+        return self._diff_bound
 
-        # TODO: handler long file names
-        # Save the pretty printed inline diffs
-        if self.inline_file.exists():
-            with self.inline_file.open(mode="r") as ilf:
-                #print(f"Reading {self.inline_file}")
-                self.inline_diff_text = ilf.read()
-        else:
-            self.inline_diff_text = "".join(difflib.unified_diff(self.a_formatted_lines, self.b_formatted_lines))
+    @property
+    def num_diffs(self):
+        return len(self.diff_bound) - 1
 
-            with self.inline_file.open(mode='w') as diff_temp:
-                #print(f"Writing {self.inline_file}")
-                diff_temp.write(self.inline_diff_text)
+    @property
+    def a_formatted(self):
+        if not self._a_formatted:
+            self._a_formatted = self.get_pretty_printed(self.a_str)
 
-    def pretty_printing(self, a_str: str, b_str: str):
+        return self.a_formatted
+
+    @property
+    def b_formatted(self):
+        if not self._b_formatted:
+            self._b_formatted = self.get_pretty_printed(self.b_str)
+
+        return self._b_formatted
+
+    @property
+    def a_formatted_range(self):
+        if not self._a_formatted_range:
+            self._a_formatted_range = get_range_offset(self.a_str, self.a_formatted)
+
+        return self._a_formatted_range
+
+    @property
+    def b_formatted_range(self):
+        if not self._b_formatted_range:
+            self._b_formatted_range = get_range_offset(self.b_str, self.b_formatted)
+
+        return self._b_formatted_range
+
+    def get_pretty_printed(self, code: str):
         """
                 Performs pretty-printing on the whole files A and B
         """
-        opts = jsbeautifier.default_options()
-        opts.indent_size = 0
-        check_or_create_dir(self.inline_proj_dir)
+        if self.extension in ['js', '.js', '.ts', '.tsx', '.jsx']:
+            import jsbeautifier
 
-        if not self.a_path_file.exists():
-            self.a_formatted = jsbeautifier.beautify(a_str, opts)
+            opts = jsbeautifier.default_options()
+            opts.indent_size = 0
 
-            with self.a_path_file.open(mode="w") as a_temp:
-                print(f"Writing {self.a_path_file}")
-                a_temp.write(self.a_formatted)
-
+            return jsbeautifier.beautify(code, opts)
         else:
-            with self.a_path_file.open(mode="r") as apf:
-                #print(f"Reading {self.a_path_file}")
-                self.a_formatted = apf.read()
+            # TODO: To be implemented for other programming languages
+            return code
 
-        if not self.b_path_file.exists():
-            self.b_formatted = jsbeautifier.beautify(b_str, opts)
-
-            with self.b_path_file.open(mode="w") as b_temp:
-                #print(f"Writing {self.b_path_file}")
-                b_temp.write(self.b_formatted)
-
-        else:
-            with self.b_path_file.open(mode="r") as bpf:
-                #print(f"Reading {self.b_path_file}")
-                self.b_formatted = bpf.read()
-
-    def add_inline_diffs(self, owner: str, project: str, version: str, file_path: str, formatted_range: List,
-                         linenos: List[int], label: str, pair_hash: str):
+    def parse_inline_diff(self, change: Change):
         """
             Adds the InlineDiff instances.
-            :param owner: The repository owner.
-            :param project: The respective project.
-            :param version: The commit sha.
-            :param file_path: The filepath of the target file.
-            :param formatted_range: The InlineDiff instance.
-            :param linenos: The InlineDiff instance.
-            :param label: The InlineDiff instance.
-            :param pair_hash: sha1 hex digest to match inline diffs and functions
+            :param change: The Change instance.
         """
-        for i, inlineno in enumerate(linenos):
-            inline_diff = InlineDiff(owner=owner, project=project, version=version, fpath=file_path, label=label,
-                                     sline=formatted_range[inlineno - 1][0], scol=formatted_range[inlineno - 1][1],
-                                     eline=formatted_range[inlineno - 1][2], ecol=formatted_range[inlineno - 1][3],
-                                     pair_hash=pair_hash)
 
-            self.inline_diffs.append(inline_diff)
+        output = {'type': change.type}
 
-    def __call__(self, unsafe_label: str) -> List[InlineDiff]:
+        if self.prettify:
+            idx = change.number - 1
+            formatted_range = self.b_formatted_range if change.type == 'addition' else self.a_formatted_range
+            output.update({'sline': formatted_range[idx][0], 'scol': formatted_range[idx][1],
+                           'eline': formatted_range[idx][2], 'ecol': formatted_range[idx][3]})
+        else:
+            output.update({'sline': change.number, 'scol': change.start_col,
+                           'eline': change.number, 'ecol': change.end_col})
+
+        return output
+
+    def __call__(self, beautify: bool = False) -> List[dict]:
         """
             Gets the corresponding row/column index range in the original text and returns a list of result entries.
             :return: A list of InlineDiff instances resulted from this inline diff.
         """
-        if not self.inline_diffs:
-            diff_bound, inline_lines, num_diffs = self.diff_bound()
 
-            for diff_id in range(num_diffs):
-                a_del_linenos, b_add_linenos, _, _ = get_row_diff_range(inline_lines, diff_bound, diff_id)
-                self.a_del_line_cnt += len(a_del_linenos)
-                self.b_add_line_cnt += len(b_add_linenos)
-                pair_hash = self.get_pair_sha(a_del_linenos, b_add_linenos)
+        results = []
 
-                self.add_inline_diffs(owner=self.entry.owner, project=self.entry.project, version=self.entry.a_version,
-                                      file_path=self.entry.diff_block.a_path, label=unsafe_label, pair_hash=pair_hash,
-                                      linenos=a_del_linenos, formatted_range=self.a_formatted_range)
+        for diff_id in range(self.num_diffs):
+            for change in get_row_diff_range(self.diff_lines, self.diff_bound, diff_id):
+                results.append(self.parse_inline_diff(change))
 
-                self.add_inline_diffs(owner=self.entry.owner, project=self.entry.project, version=self.entry.b_version,
-                                      file_path=self.entry.diff_block.b_path, label='safe', pair_hash=pair_hash,
-                                      linenos=b_add_linenos, formatted_range=self.b_formatted_range)
-
-        return self.inline_diffs
-
-    def get_pair_sha(self, a_del_lines: list, b_add_lines: list):
-        sha = hashlib.sha1()
-        sha_str = f'{self.entry.owner}_{self.entry.project}_{self.entry.diff_block.a_path}'
-        a_del_lines_str = '_'.join([str(l) for l in a_del_lines])
-        sha_str += f'_{self.entry.a_version}_{a_del_lines_str}'
-        b_add_lines_str = '_'.join([str(l) for l in b_add_lines])
-        sha_str += f'_{self.entry.b_version}_{b_add_lines_str}'
-        sha.update(sha_str.encode())
-        return sha.hexdigest()
-
-    def calc_sim_ratio(self) -> float:
-        """
-            Computes and records the similarity ratio between the two files from the parent and the later
-            commit at line-level (after pretty-printing). Prints out a notification message if the ratio is
-            lower than a specified threshold.
-            # >>> Ratio = 1. - (a_only + b_only) / (a_total + b_total)
-            #     a_only: The number of deleted lines in file A.
-            #     b_only: The number of added lines in file B.
-            #     a_total: The total number of lines in file A.
-            #     b_total: The total number of lines in file B.
-        """
-        if not self.sim_ratio:
-            total_lines = self.size_a_lines + self.size_b_lines
-            self.sim_ratio = 1. - (self.a_del_line_cnt + self.b_add_line_cnt) / total_lines
-
-        return self.sim_ratio
-
-    def __str__(self):
-        return f"{self.entry.a_version},{self.entry.diff_block.a_path},{self.entry.b_version}," \
-               f"{self.entry.diff_block.b_path},{self.sim_ratio}"
-
-    def warning(self) -> str:
-        return f"\nNote: line-level similarity ratio = {self.sim_ratio}, a:-{self.a_del_line_cnt}/{self.size_a_lines}," \
-               f"b:+{self.b_add_line_cnt}/{self.size_b_lines}\na: {self.entry.a_version},{self.entry.diff_block.a_path}\n" \
-               f"b: {self.entry.b_version},{self.entry.diff_block.b_path}\n "
+        return results
--- tenet/data/diff.py
+++ tenet/data/diff.py
@@ -1,6 +1,34 @@
 import pandas as pd
+
 from dataclasses import dataclass
 from pathlib import Path
+
+
+@dataclass
+class Change:
+    content: str
+    number: int
+    type: str
+
+    @property
+    def start_col(self):
+        # TODO: This is a temporary solution. Need to find a better way to calculate the start column
+        line_with_spaces = self.content.expandtabs(4)
+        return (len(line_with_spaces) - len(line_with_spaces.lstrip())) + 1
+
+    @property
+    def end_col(self):
+        return len(self.content) - 1
+
+
+@dataclass
+class Addition(Change):
+    type: str = 'addition'
+
+
+@dataclass
+class Deletion(Change):
+    type: str = 'deletion'
 
 
 @dataclass
